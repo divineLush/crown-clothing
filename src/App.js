83036820f5c26d893b2abf83bc72e748596582c7
divineLush <homeoflucifer@gmail.com>
2021-01-16 02:40:23 -0500

    basic router setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.jsx';
 
 import './App.css';
 
+const HatsPage = () => (
+  <section><h1>Hats page</h1></section>
+);
+
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={ HomePage } />
+        <Route path="/hats" component={ HatsPage } />
+      </Switch>
     </div>
   );
 }
