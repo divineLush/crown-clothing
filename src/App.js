80a963b53bced0eaef8271943835832423ec40e3
divineLush <homeoflucifer@gmail.com>
2021-01-17 05:51:13 -0500

    storing currentUser

--- src/App.js
+++ src/App.js
@@ -6,17 +6,40 @@ import HomePage from './pages/home/home.jsx';
 import ShopPage from './pages/shop/shop.jsx';
 import SignInSignUpPage from './pages/sign-in/sign-in.jsx';
 
+import { auth } from './firebase/firebase.utils.js';
+
 import './App.css';
 
-const App = () => (
-  <div className="App">
-    <Header />
-    <Switch>
-      <Route exact path="/" component={ HomePage } />
-      <Route path="/shop" component={ ShopPage } />
-      <Route path="/signin" component={ SignInSignUpPage } />
-    </Switch>
-  </div>
-);
+class App extends React.Component {
+  state = {
+    currentUser: null,
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount () {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(currentUser => {
+      this.setState({ currentUser });
+    });
+  }
+
+  componentWillUnmount () {
+    // closing subscription when app unmounts
+    this.unsubscribeFromAuth();
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={ HomePage } />
+          <Route path="/shop" component={ ShopPage } />
+          <Route path="/signin" component={ SignInSignUpPage } />
+        </Switch>
+      </div>
+    );
+  }
+}
 
 export default App;
