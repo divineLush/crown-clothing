21e643a4346c55d6f5cc20b085b507a7c370b245
divineLush <homeoflucifer@gmail.com>
2021-01-17 06:49:08 -0500

    sign out btn styles

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,24 +1,46 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { auth } from '../../firebase/firebase.utils.js';
+
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.scss';
 
-const Header = () => (
-  <header className="header">
-    <Link className="header__logo-link" to="/">
-      <Logo className="header__logo-image" />
-    </Link>
-    <nav className="header__options">
-      <Link className="header__nav-link" to="/shop">
-        SHOP
+const Header = ({ currentUser }) => {
+  const signLink = (() => {
+    const signOut = (
+      <button
+        className="header__sign-out-btn"
+        onClick={ () => { auth.signOut(); } }
+      >
+        SIGN OUT
+      </button>
+    );
+
+    const signIn = (
+      <Link className="header__nav-link" to="/signin">SIGN IN</Link>
+    );
+
+    return currentUser ? signOut : signIn
+  })();
+
+  return (
+    <header className="header">
+      <Link className="header__logo-link" to="/">
+        <Logo className="header__logo-image" />
       </Link>
-      <Link className="header__nav-link" to="/shop">
-        CONTACT
-      </Link>
-    </nav>
-  </header>
-);
+      <nav className="header__options">
+        <Link className="header__nav-link" to="/shop">
+          SHOP
+        </Link>
+        <Link className="header__nav-link" to="/shop">
+          CONTACT
+        </Link>
+        { signLink }
+      </nav>
+    </header>
+  );
+}
 
 export default Header;
