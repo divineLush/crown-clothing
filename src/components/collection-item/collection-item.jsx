12873099e1b1753d6fd19827e7b326bae3f4e29c
Вишняков Алексей Анатольевич <a.vishnyakov@e2e4online.ru>
2021-01-22 02:11:56 -0500

    add to cart button

--- src/components/collection-item/collection-item.jsx
+++ src/components/collection-item/collection-item.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Button from '../button/button.jsx';
+
 import './collection-item.scss';
 
 const CollectionItem = ({ id, name, price, imageUrl }) => (
@@ -12,6 +14,7 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
       <span className='collection-item__name'>{ name }</span>
       <span className='collection-item__price'>{ price }</span>
     </footer>
+    <Button modifier="inverted">Add to cart</Button>
   </article>
 );
 
