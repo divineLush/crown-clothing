38bb3f50c0d000f4f6832b5f376556c283ca0f22
Вишняков Алексей Анатольевич <a.vishnyakov@e2e4online.ru>
2021-02-10 00:29:24 -0500

    data normalisation & collection page

--- src/pages/collection/collection.jsx
+++ src/pages/collection/collection.jsx
@@ -7,11 +7,21 @@ import CollectionItem from '../../components/collection-item/collection-item.jsx
 
 import './collection.scss';
 
-const CollectionPage = ({ collection,  match }) => (
-  <section className="collection">
-    <h2>{ collection.title }</h2>
-  </section>
-);
+const CollectionPage = ({ collection,  match }) => {
+  const { title, items } = collection;
+  const itemsList = items.map((item, key) => (
+    <CollectionItem key={ key } item={ item } />
+  ));
+
+  return (
+    <section className="collection-page">
+      <h2 className="collection-page__title">{ title }</h2>
+      <section className="collection-page__items">
+        { itemsList }
+      </section>
+    </section>
+  );
+}
 
 const mapStateToProps = (state, ownProps) => ({
   collection: selectCollectionByUrl(ownProps.match.params.collectionId)(state),
